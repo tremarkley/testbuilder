05fc892011e8d2221b94d4e9ecb86224875d1d20
Harry Markley <tremarkley@gmail.com>
2017-12-16 18:47:48 -0800

    Detect American Express and Diner's Club Network

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,9 +9,15 @@
 
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
+  var firstTwoNumbers = cardNumber[0] + cardNumber[1];
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+  if (firstTwoNumbers === '38' || firstTwoNumbers === '39' & cardNumber.length === 14) {
+  	return 'Diner\'s Club';
+  }
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
+  else if (firstTwoNumbers === '34' || firstTwoNumbers === '37' & cardNumber.length === 15) {
+  	return 'American Express';
+  }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
 
